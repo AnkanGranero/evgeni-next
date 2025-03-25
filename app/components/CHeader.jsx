d7b4ff46d29fef22a5f3d7f4367087bd29ef8a5d
Andreas Granér <andreas@graner.me>
2025-03-25 13:51:26 +0100

    old design

--- app/components/CHeader.jsx
+++ app/components/CHeader.jsx
@@ -5,10 +5,12 @@ export default async function CHeader() {
     const headerMenuItems = await getHeaderMenuItems();
 
     return (
-        <header className="border-b border-gray-200 h-[8rem] flex">
-        <nav className="w-full mx-auto flex justify-center items-center flex-col h-32">
-          <h1 className="font-bold grow-2 text-6xl">Evgeni Leonov</h1>
-            <ul className="flex h-1/2 w-full bg-green-300 p-3">
+        <header className="border-b border-gray-200 ">
+        <nav className="w-full mx-auto flex justify-center items-center flex-col h-32 h-[10rem] flex">
+          <div className="px-0 h-3/5 flex items-center justify-center bg-[#ead74c] w-full">
+          <h1 className="font-bold text-6xl leading-[1.5] text-stone-950">Evgeni Leonov</h1>
+          </div>
+            <ul className="flex h-2/5 w-full bg-green-300 p-3">
               {headerMenuItems.map((item, index) => (
                 <li key={index} className="h-full ml-4">
                   <a href={item.link}>
