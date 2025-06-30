3449c773b10db3316791b35cccfab888defb41c9
Andreas Granér <andreas@graner.me>
2025-06-30 16:03:10 +0200

    semantic fix Hero

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -23,14 +23,14 @@ export default function Hero({ text }) {
   return (
     <animated.div ref={containerRef} className="min-h-screen">
 
-          <animated.div style={{backgroundSize: scrollYProgress.to(v=> `${backgroundStartSize  - (v*10)}%`), ...styles}} className='bg-center min-h-[100vh] md:min-h-screen w-full bg-no-repeat bg-[url(/Evgeni_Leonov_foto.jpg)]' />
+      <animated.div style={{ backgroundSize: scrollYProgress.to(v => `${backgroundStartSize - (v * 10)}%`), ...styles }} className='bg-center min-h-[100vh] md:min-h-screen w-full bg-no-repeat bg-[url(/Evgeni_Leonov_foto.jpg)]' />
 
-          <animated.section style={{opacity: scrollYProgress.to(v => 1 + v), transform: scrollYProgress.to(v => `translateY(-${v * 250}px)`)}} className="flex justify-center w-full bg-test-500">
-            <div className="text-white text-center max-w-5xl" >
-              <div className='md:text-3xl p-8'>{text}</div>
-            </div>
-          </animated.section>
-  
+      <animated.section style={{ opacity: scrollYProgress.to(v => 1 + v), transform: scrollYProgress.to(v => `translateY(-${v * 250}px)`) }} className="flex justify-center w-full bg-test-500">
+        <article className="text-white text-center max-w-5xl md:text-3xl p-8" >
+          {text}
+        </article>
+      </animated.section>
+
 
 
 
