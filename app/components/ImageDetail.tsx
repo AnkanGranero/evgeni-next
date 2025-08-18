2892a71a60e28e02a2a1b7605e5ce25787cd1ec4
Andreas Granér <Andreas@graner.me>
2025-08-18 19:33:59 +0200

    fix: dynamic width and height for Image component

--- app/components/ImageDetail.tsx
+++ app/components/ImageDetail.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 export default function ImageDetail({ image }: { image: GalleryImage }) {
   return (
     <div className="space-y-3">
-      <Image src={image.fullUrl} alt={image.id} className=" mx-auto block max-w-full max-h-[90vh] rounded-xl object-contain" width="300" height="300"/>
+      <Image src={image.fullUrl} alt={image.id} className=" mx-auto block max-w-full max-h-[90vh] rounded-xl object-contain" width={image.w} height={image.w}/>
     </div>
   );
 }
