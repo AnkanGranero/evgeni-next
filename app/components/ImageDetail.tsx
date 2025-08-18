c454186976c23d3c9c93f151f47dc86cf3a57042
Andreas Granér <Andreas@graner.me>
2025-08-18 11:09:15 +0200

    fix: remove to show image id in UI, added li element in gallery for better semantics, removed obsolete code in Modal.tsx

--- app/components/ImageDetail.tsx
+++ app/components/ImageDetail.tsx
@@ -4,7 +4,6 @@ export default function ImageDetail({ image }: { image: GalleryImage }) {
   return (
     <div className="space-y-3">
       <img src={image.fullUrl} alt={image.id} className=" mx-auto block max-w-full max-h-[90vh] rounded-xl object-contain" />
-      <div className="text-sm text-gray-600">Bild-ID: {image.id}</div>
     </div>
   );
 }
--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,20 +1,28 @@
-import { getGalleryImages } from "@/lib/contentful/gallery"
-import Link from "next/link";
-import Image from "next/image";
+import { getGalleryImages } from '@/lib/contentful/gallery';
+import Link from 'next/link';
+import Image from 'next/image';
 
 export default async function Page() {
+  const images = await getGalleryImages();
 
-    const images = await getGalleryImages();
-
-    return (
-        <ul className="mt-[8rem] grid grid-cols-2 md:grid-cols-5 gap-2 p-0 list-none">
-            {images.map(image => (
-                <Link className="aspect-square overflow-hidden cursor-pointer" key={image.id} href={`/gallery/${image.id}`}>
-                    <Image className="w-full h-full object-cover block" src={image.fullUrl} width={500}
-                        height={500}
-                        alt="Picture of the author" />
-                </Link>
-            ))}
-        </ul>
-    )
+  return (
+    <ul className="mt-[8rem] grid grid-cols-2 md:grid-cols-5 gap-2 p-0 list-none">
+      {images.map((image) => (
+        <li key={image.id}>
+          <Link
+            className="aspect-square overflow-hidden cursor-pointer"
+            href={`/gallery/${image.id}`}
+          >
+            <Image
+              className="w-full h-full object-cover block"
+              src={image.fullUrl}
+              width={500}
+              height={500}
+              alt="Picture of the author"
+            />
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 }
