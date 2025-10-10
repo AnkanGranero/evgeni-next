e27b936baa47164e1290f1cd66f9d06996e845e5
Andreas Granér <andreas@graner.me>
2025-10-10 15:50:19 +0200

    add parallax-text component, add framer-motion and lenis for creating parallax effect

---
+++ app/components/parallax-text.tsx
@@ -0,0 +1,61 @@
+"use client";
+import { ReactNode, useEffect, useRef } from "react";
+import type { LenisRef } from 'lenis/react';
+import { useTransform, useScroll, motion, useSpring, cancelFrame, frame } from "framer-motion";
+import {  ReactLenis,  } from "lenis/react";
+
+export default function ParallaxText({
+  text,
+  background,
+}: {
+  text: ReactNode;
+  background: string;
+}) {
+
+  const lenisRef = useRef<LenisRef>(null)
+
+  useEffect(() => {
+    function update(data: { timestamp: number }) {
+      const time = data.timestamp
+      lenisRef.current?.lenis?.raf(time)
+    }
+
+    frame.update(update, true)
+
+    return () => cancelFrame(update)
+  }, [])
+
+
+  const textContainer = useRef(null);
+  const { scrollYProgress } = useScroll({ target: textContainer });
+  const yText = useTransform(scrollYProgress, [0, 1], [0, -100]);
+  const ySpringText = useSpring(yText, {
+    stiffness: 105,
+    damping: 20,
+  });
+  const yBG = useTransform(scrollYProgress, [0, 1], [0, 600]);
+
+  const opacity = useTransform(scrollYProgress, [0, 1], [1, 0]);
+
+  return (
+    <>
+       <ReactLenis root options={{ autoRaf: false,  lerp: 0.1,
+  wheelMultiplier: 0.6, }} ref={lenisRef} />
+      <section
+        className="h-full relative isolate flex justify-center overflow-hidden"
+        ref={textContainer}
+      >
+        <motion.div
+          className={`absolute -z-10 inset-0 bg-[url(${background})] bg-top bg-[length:150%] 2xl:bg-[length:110%] bg-no-repeat`}
+          style={{ y: yBG, opacity }}
+        />
+        <motion.article
+          className="relative z-10 text-white text-center max-w-5xl md:text-3xl p-8 w-full pt-[70rem]"
+          style={{ y: ySpringText }}
+        >
+          {text}
+        </motion.article>
+      </section>
+    </>
+  );
+}
