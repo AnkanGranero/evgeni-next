93e02d284f9591b80b6d9a07cbcf07676fe60336
Andreas Granér <andreas@graner.me>
2025-10-16 17:06:21 +0200

    Fix: Eslint errors

--- lib/contentful/latest/types.ts
+++ lib/contentful/latest/types.ts
@@ -1,5 +1,5 @@
-import { Asset, Entry, EntryFields, EntrySkeletonType } from "contentful";
-import { CFAsset, CFImageFile } from "../types";
+import { Entry, EntryFields, EntrySkeletonType } from "contentful";
+import {  CFImageFile } from "../types";
 import type { Document as CfDocument } from "@contentful/rich-text-types";
 
 
