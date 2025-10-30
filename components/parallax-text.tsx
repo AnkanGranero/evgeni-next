fd3e8db1d1166a4bcea09708d532cce87a6e3148
Andreas Granér <Andreas@graner.me>
2025-10-30 14:01:16 +0100

    Fix: style fixes on startpage parallax text, clean up global css

--- components/parallax-text.tsx
+++ components/parallax-text.tsx
@@ -1,8 +1,8 @@
-"use client";
-import { ReactNode, useEffect, useRef } from "react";
+'use client';
+import { ReactNode, useEffect, useRef } from 'react';
 import type { LenisRef } from 'lenis/react';
-import { useTransform, useScroll, motion, useSpring, cancelFrame, frame } from "framer-motion";
-import {  ReactLenis,  } from "lenis/react";
+import { useTransform, useScroll, motion, useSpring, cancelFrame, frame } from 'framer-motion';
+import { ReactLenis } from 'lenis/react';
 
 export default function ParallaxText({
   text,
@@ -11,20 +11,18 @@ export default function ParallaxText({
   text: ReactNode;
   background: string;
 }) {
-
-  const lenisRef = useRef<LenisRef>(null)
+  const lenisRef = useRef<LenisRef>(null);
 
   useEffect(() => {
     function update(data: { timestamp: number }) {
-      const time = data.timestamp
-      lenisRef.current?.lenis?.raf(time)
+      const time = data.timestamp;
+      lenisRef.current?.lenis?.raf(time);
     }
 
-    frame.update(update, true)
+    frame.update(update, true);
 
-    return () => cancelFrame(update)
-  }, [])
-
+    return () => cancelFrame(update);
+  }, []);
 
   const textContainer = useRef(null);
   const { scrollYProgress } = useScroll({ target: textContainer });
@@ -39,18 +37,26 @@ export default function ParallaxText({
 
   return (
     <>
-       <ReactLenis root options={{ autoRaf: false,  lerp: 0.1,
-  wheelMultiplier: 0.6, }} ref={lenisRef} />
+      <ReactLenis
+        root
+        options={{
+          autoRaf: false,
+          lerp: 0.1,
+          wheelMultiplier: 0.6,
+          touchMultiplier: 0.6,
+        }}
+        ref={lenisRef}
+      />
       <section
-        className="h-full relative isolate flex justify-center overflow-hidden"
+        className="h-full content-grid relative isolate flex justify-center overflow-hidden"
         ref={textContainer}
       >
         <motion.div
-          className={`absolute -z-10 inset-0 bg-[position:center_7%] lg:bg-top bg-[length:200%] 2xl:bg-[length:110%] bg-no-repeat`}
+          className={`full absolute -z-10 inset-0 bg-[position:center_7%] lg:bg-top bg-[length:200%] 2xl:bg-[length:110%] bg-no-repeat`}
           style={{ y: yBG, opacity, backgroundImage: `url(${background})` }}
         />
         <motion.article
-          className="relative z-10 text-white text-center max-w-5xl lg:text-3xl p-8 w-full pt-[30rem] md:pt-[40rem] xl:pt-[50rem] 4xl:pt-[70rem]"
+          className="relative z-10 popout text-white lg:text-3xl p-8 w-full pt-[25rem] md:pt-[40rem] xl:pt-[50rem] 4xl:pt-[70rem]"
           style={{ y: ySpringText }}
         >
           {text}
