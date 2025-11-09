ffa3d3f8b409d09af56ead8e1495b26bbd2d4adf
Andreas Granér <andreas@graner.me>
2025-11-09 17:05:13 +0100

    Fix: sorted CvItems after orderNumber

--- app/cv/page.tsx
+++ app/cv/page.tsx
@@ -3,16 +3,26 @@ import { renderRichText } from "@/lib/contentful/rich-text";
 
 export default async function CVPage() {
   const cvItems = await fetchCVItems();
-
+  const sortedCvItems = cvItems
+    ? [...cvItems].sort((a, b) => {
+        const aNum = a.fields?.orderNumber ?? 9999;//fallback as last if orderNumber is missing
+        const bNum = b.fields?.orderNumber ?? 9999;
+        return aNum - bNum;
+      })
+    : [];
   return (
     <main className="content-grid">
       <ul className="mt-50 mb-20">
         {cvItems
-          ? cvItems.map((item) => {
+          ? sortedCvItems.map((item) => {
               return (
-                
-                <li key={item.fields.orderNumber} className="text-white text-2xl mb-20">
-                    <h3 className="text-3xl text-center mb-8">{item.fields.category}</h3>
+                <li
+                  key={item.fields.orderNumber}
+                  className="text-white text-2xl mb-20"
+                >
+                  <h3 className="text-3xl text-center mb-8">
+                    {item.fields.category}
+                  </h3>
                   {renderRichText(item.fields.text)}
                 </li>
               );
